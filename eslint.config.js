1bd8771711371ac84e88fa823a90c6d113587610
Måns Jackson <mans.jackson@gmail.com>
2026-02-18 16:39:25 +0100

    fix deps

--- eslint.config.js
+++ eslint.config.js
@@ -2,14 +2,12 @@ import js from '@eslint/js';
 import unusedImports from 'eslint-plugin-unused-imports';
 import tseslint from 'typescript-eslint';
 import tseslintparser from '@typescript-eslint/parser';
-import pluginPromise from 'eslint-plugin-promise';
 import prettierPlugin from 'eslint-plugin-prettier/recommended';
 import prettierConfig from 'eslint-config-prettier';
 
 export default [
   js.configs.recommended,
   ...tseslint.configs.recommended,
-  pluginPromise.configs['flat/recommended'],
   prettierPlugin,
   prettierConfig,
 
